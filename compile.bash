35388fb2e5b63f1f298350541f20aa5726cf24ca
DevJH <hkonly@korea.ac.kr>
2022-03-16 16:49:06 +0900

    update

--- compile.bash
+++ compile.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-g++ source/async_img_to_bin.cpp -o exes/async.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
-g++ source/sync_img_to_bin.cpp -o exes/sync.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
-g++ source/imagetest.cpp -o exes/savetest.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
+mkdir out
+
+g++ source/async_img_to_bin.cpp -o out/async.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
+g++ source/sync_img_to_bin.cpp -o out/sync.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
+g++ source/imagetest.cpp -o out/savetest.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
