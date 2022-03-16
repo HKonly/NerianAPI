26aeea3f30f3346c1afee0b56d611e89660a298a
DevJH <hkonly@korea.ac.kr>
2022-03-16 11:43:58 +0900

    update

--- compile.bash
+++ compile.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-g++ source/async_img_to_bin.cpp -o async.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
-g++ source/sync_img_to_bin.cpp -o sync.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
+g++ source/async_img_to_bin.cpp -o exes/async.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
+g++ source/sync_img_to_bin.cpp -o exes/sync.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
+g++ source/imagetest.cpp -o exes/savetest.out -Wall -L./lib -I./include -lvisiontransfer -pthread -Wl,-rpath ./lib
--- source/imagetest.cpp
+++ source/imagetest.cpp
@@ -1,57 +1,67 @@
- #include <visiontransfer/deviceenumeration.h>
- #include <visiontransfer/asynctransfer.h>
- #include <visiontransfer/imageset.h>
- #include <iostream>
- #include <exception>
- #include <stdio.h>
+#include <visiontransfer/deviceenumeration.h>
+#include <visiontransfer/asynctransfer.h>
+#include <visiontransfer/imageset.h>
+#include <iostream>
+#include <exception>
+#include <stdio.h>
+#include <chrono>
   
- using namespace visiontransfer;
+using namespace visiontransfer;
+using namespace std;
+using namespace chrono;
   
- int main() {
-     try {
-         // Search for Nerian stereo devices
-         DeviceEnumeration deviceEnum;
-         DeviceEnumeration::DeviceList devices = deviceEnum.discoverDevices();
-         if(devices.size() == 0) {
-             std::cout << "No devices discovered!" << std::endl;
-             return -1;
-         }
+int main() {
+    system_clock::time_point start, end;
+    try {
+        // Search for Nerian stereo devices
+        DeviceEnumeration deviceEnum;
+        DeviceEnumeration::DeviceList devices = deviceEnum.discoverDevices();
+        if(devices.size() == 0) {
+            printf("No devices discovered!\n");
+            return -1;
+        }
+ 
+        // Print devices
+        printf("Discovered devices:\n");
+        for(unsigned int i = 0; i< devices.size(); i++) {
+            printf("%s\n", devices[i].toString().c_str());
+        }
+        printf("\n");
+ 
+        // Create an image transfer object that receives data from
+        // the first detected device
+        AsyncTransfer asyncTransfer(devices[0]);
+
+        start = system_clock::now();
+ 
+        // Receive 100 images
+        for(int i=0; i<100; i++) {
+            printf("Receiving image set %d\n", i);
+ 
+            // Receive image
+            ImageSet imageSet;
+            while(!asyncTransfer.collectReceivedImageSet(imageSet,
+                0.1 /*timeout*/)) {
+                // Keep on trying until reception is successful
+            }
+ 
+            // Write all included images one after another
+            for(int imageNumber = 0; imageNumber < imageSet.getNumberOfImages(); imageNumber++) {
+                // Create PGM file
+                char fileName[100];
+                snprintf(fileName, sizeof(fileName), "save_images/image%03d_%d.pgm", i,
+                    imageNumber);
+ 
+                imageSet.writePgmFile(imageNumber, fileName);
+            }
+        }
+
+        end = system_clock::now();
+        chrono::milliseconds elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+        printf("Time elapsed: %ld\n", elapsed.count());
+    } catch(const std::exception& ex) {
+        cerr << "Exception occurred: " << ex.what() << endl;
+    }
   
-         // Print devices
-         std::cout << "Discovered devices:" << std::endl;
-         for(unsigned int i = 0; i< devices.size(); i++) {
-             std::cout << devices[i].toString() << std::endl;
-         }
-         std::cout << std::endl;
-  
-         // Create an image transfer object that receives data from
-         // the first detected device
-         AsyncTransfer asyncTransfer(devices[0]);
-  
-         // Receive 100 images
-         for(int i=0; i<100; i++) {
-             std::cout << "Receiving image set " << i << std::endl;
-  
-             // Receive image
-             ImageSet imageSet;
-             while(!asyncTransfer.collectReceivedImageSet(imageSet,
-                 0.1 /*timeout*/)) {
-                 // Keep on trying until reception is successful
-             }
-  
-             // Write all included images one after another
-             for(int imageNumber = 0; imageNumber < imageSet.getNumberOfImages(); imageNumber++) {
-                 // Create PGM file
-                 char fileName[100];
-                 snprintf(fileName, sizeof(fileName), "image%03d_%d.pgm", i,
-                     imageNumber);
-  
-                 imageSet.writePgmFile(imageNumber, fileName);
-             }
-         }
-     } catch(const std::exception& ex) {
-         std::cerr << "Exception occurred: " << ex.what() << std::endl;
-     }
-  
-     return 0;
- }
+    return 0;
+}
